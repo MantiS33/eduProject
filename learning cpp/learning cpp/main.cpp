927a6495c3e00abedb08c44ba2a372f7f753655a
Ivan Zykov <bendr2000@gmail.com>
2022-06-18 11:41:16 +0500

    MantiS
    Testing commit

---
+++ learning cpp/learning cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learning cpp
+//
+//  Created by Иван Зыков on 04.06.2022.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n"s << endl;
+    return 0;
+}
