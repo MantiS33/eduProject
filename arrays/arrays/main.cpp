b93971715bf7162c5ef2d36786e742301d5400a9
Ivan Zykov <bendr2000@gmail.com>
2022-07-02 23:17:24 +0500

    .

--- arrays/arrays/main.cpp
+++ arrays/arrays/main.cpp
@@ -9,9 +9,11 @@
 using namespace std;
 
 template<class T>
-class List{
+class List
+{
 private:
-    struct node    {
+    struct node
+    {
         T data;
         node *next;
         node *prev;
@@ -53,7 +55,8 @@ public:
     void clear();
     
     //перегрузка вывода
-    friend ostream &operator<<(ostream &out, List &elem){
+    friend ostream &operator<<(ostream &out, List &elem)
+    {
         elem.tmp = elem.head;
         while(elem.tmp != NULL)
         {
@@ -64,18 +67,21 @@ public:
     }
     
     //перегрузка ввода
-    friend istream &operator>>(istream &in, List &elem){
+    friend istream &operator>>(istream &in, List &elem)
+    {
         elem.tmp = new node;
         elem.tmp->next = NULL;
         in >> elem.tmp->data;
                 
-        if(elem.head != NULL){
+        if(elem.head != NULL)
+        {
             elem.tail->next = elem.tmp;
             elem.tmp->prev = elem.tail;
             elem.tail = elem.tmp;
             elem.count += 1;
         }
-        else{
+        else
+        {
             elem.tmp->prev = NULL;
             elem.head = elem.tail = elem.tmp;
             elem.count += 1;
@@ -87,7 +93,8 @@ public:
 
 //конструктор по умолчанию
 template<class T>
-List<T>::List(){
+List<T>::List()
+{
     head = NULL;
     tail = NULL;
     tmp = NULL;
@@ -96,26 +103,30 @@ List<T>::List(){
 
 //деструктор
 template<class T>
-List<T>::~List(){
+List<T>::~List()
+{
     clear();
 }
 
 //добваление элемента, добавляется в конец
 template<class T>
-void List<T>::add(T addData){
+void List<T>::add(T addData)
+{
     
     tmp = new node;
     tmp->data = addData;
     tmp->next = NULL;
     
-    if(head != NULL){
+    if(head != NULL)
+    {
         
         tail->next = tmp;
         tmp->prev = tail;
         tail = tmp;
         count += 1;
     }
-    else{
+    else
+    {
         
         tmp->prev = NULL;
         head = tail = tmp;
@@ -125,10 +136,12 @@ void List<T>::add(T addData){
 
 //вывести все элементы
 template<class T>
-void List<T>::show(){
+void List<T>::show()
+{
     
     tmp = head;
-    while(tmp != NULL){
+    while(tmp != NULL)
+    {
         
         cout << tmp->data << endl;
         tmp=tmp->next;
@@ -137,18 +150,21 @@ void List<T>::show(){
 
 //вывести количество элементов в списке
 template<class T>
-int List<T>::getCount(){
+int List<T>::getCount()
+{
     return count;
 }
 
 //вставить в определенную позицию
 template<class T>
-void List<T>::insert(unsigned int pos, T addData){
+void List<T>::insert(unsigned int pos, T addData)
+{
     node *tmpInsert = new node;
     tmpInsert->data = addData;
     tmp = head;
     int counter = 1;
-    while(tmp != NULL){
+    while(tmp != NULL)
+    {
         if(pos == counter && tmp == head){
             tmpInsert->prev = NULL;
             tmpInsert->next = head;
@@ -156,14 +172,16 @@ void List<T>::insert(unsigned int pos, T addData){
             count += 1;
             return;
         }
-        else if(pos >= counter && tmp == tail){
+        else if(pos >= counter && tmp == tail)
+        {
             tmpInsert->prev = tail;
             tmpInsert->next = NULL;
             tail->next = tmpInsert;
             count += 1;
             return;
         }
-        else if(pos == counter && (tmp != head || tmp != tail)){
+        else if(pos == counter && (tmp != head || tmp != tail))
+        {
             tmpInsert->prev = tmp->prev;
             tmpInsert->next = tmp->next;
             tmp->prev->next = tmpInsert;
@@ -177,25 +195,30 @@ void List<T>::insert(unsigned int pos, T addData){
 
 //удалить по значению
 template<class T>
-void List<T>::removeByValue(T value){
+void List<T>::removeByValue(T value)
+{
     
 }
 
 //удалить по позиции
 template<class T>
-void List<T>::removeByCount(unsigned int pos){
+void List<T>::removeByCount(unsigned int pos)
+{
     
 }
 
 //удалить голову
 template<class T>
-void List<T>::removeHead(){
-    if(head == tail){
+void List<T>::removeHead()
+{
+    if(head == tail)
+    {
         tail = NULL;
         delete head;
         count = 0;
     }
-    else{
+    else
+    {
         tmp = head;
         head = head->next;
         head->prev = NULL;
@@ -207,13 +230,16 @@ void List<T>::removeHead(){
 
 //удалить конец
 template<class T>
-void List<T>:: removeTail(){
-    if(head == tail){
+void List<T>:: removeTail()
+{
+    if(head == tail)
+    {
         tail = NULL;
         delete head;
         count = 0;
     }
-    else{
+    else
+    {
         tmp = tail;
         tail = tail->prev;
         tail->next = NULL;
@@ -224,15 +250,20 @@ void List<T>:: removeTail(){
 
 //очистить список
 template<class T>
-void List<T>::clear(){
-    if(head != NULL){
-        if(head == tail){
+void List<T>::clear()
+{
+    if(head != NULL)
+    {
+        if(head == tail)
+        {
             tail = NULL;
             delete head;
         }
-        else{
+        else
+        {
             tmp = head;
-            while(tmp != NULL){
+            while(tmp != NULL)
+            {
                 head = head->next;
                 delete tmp;
                 tmp = head;
@@ -242,7 +273,8 @@ void List<T>::clear(){
 }
 
 
-int main(int argc, const char * argv[]){
+int main(int argc, const char * argv[])
+{
     List<int> listArray;
     listArray.add(10);
     listArray.add(30);
